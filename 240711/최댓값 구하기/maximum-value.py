f86a5ca4835751650f787a1f3334d6434e2542a4
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-07-11 17:46:55 +0900

    Title: 최댓값 구하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240711/최댓값 구하기/maximum-value.py
+++ 240711/최댓값 구하기/maximum-value.py
@@ -1,3 +1,12 @@
 a,b,c = map(int,input().split())
 
-print(max(a,b,c))
+if a >= b:
+    if a >= c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b >= c:
+        print(b)
+    else:
+        print(c)
