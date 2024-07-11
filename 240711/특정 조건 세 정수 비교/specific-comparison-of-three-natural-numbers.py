4720d53a9ff8fae65f12cb60f2bd3a7b75842604
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-07-11 16:43:09 +0900

    Title: 특정 조건 세 정수 비교, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,13 @@
+a,b,c = map(int,input().split())
+
+min_ = min(a,b,c)
+
+if a == min_:
+    print(1, end = "")
+else:
+    print(0, end = " ")
+
+if a == b and b == c and a == c:
+    print(1, end =" ")
+else:
+    print(0, end =" ")
