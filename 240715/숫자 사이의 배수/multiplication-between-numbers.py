1fa3476c2b766331e43bc010e94b3709bfe501cd
choihabin <154362206+haaaabin@users.noreply.github.com>
2024-07-15 13:45:46 +0900

    Title: 숫자 사이의 배수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240715/숫자 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,8 @@
+a,b = map(int,input().split())
+sum_val = 0
+cnt = 0
+for i in range(a,b+1):
+    if i % 5 == 0 or i % 7 == 0:
+        cnt +=1
+        sum_val += i
+print(f"{sum_val} {(sum_val / cnt):.1f}" )
